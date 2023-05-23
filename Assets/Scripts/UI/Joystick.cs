a789109b01bdf83ba732ec7ec3db0036ed6a46c3
Guma178 <69guma91@gmail.com>
2023-05-23 19:29:40 +0300

    Radar and bilboard
    Radar drawer and bilboard aligner implemented

--- Assets/Scripts/UI/Joystick.cs
+++ Assets/Scripts/UI/Joystick.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-namespace TCF.UI
+namespace SARPG.UI
 {
     public class Joystick : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
     {
